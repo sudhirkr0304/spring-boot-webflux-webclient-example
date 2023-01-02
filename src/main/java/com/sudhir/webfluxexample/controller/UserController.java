cb3d4448bea86874c30526f6395e532a9280c8f6
suku0721 <Samiksha@0733>
2023-01-02 23:27:48 +0530

    updated usercontroller

--- src/main/java/com/sudhir/webfluxexample/controller/UserController.java
+++ src/main/java/com/sudhir/webfluxexample/controller/UserController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.soap.SOAPBinding;
+
 import java.util.List;
 
 @RestController
